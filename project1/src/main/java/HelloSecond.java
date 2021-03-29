d0f881d3db602c26972aedaaf41d679bcb4002b6
DaniilShmelev <v-dshmelev@microsoft.com>
2021-03-29 07:59:42 +0300

    add project

---
+++ project1/src/main/java/HelloSecond.java
@@ -0,0 +1,8 @@
+public class HelloSecond {
+  
+  public static String GREETING = "Hello world 2!";
+  
+  public static void main(String []args) {
+    
+  }  
+}
---
+++ project2/src/test/java/HelloSecondTests.java
@@ -0,0 +1,15 @@
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class HelloSecondTests {
+    @Test
+    public void addition_isCorrect2() {
+        assertEquals(4, 2 + 2);
+    }
+}
